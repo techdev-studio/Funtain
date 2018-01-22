4ce6f85a2af68186faa5f972f14a3cf9e3d3b071
first commit <mikban152@mikban152Mac.local>
2018-01-21 22:53:54 -0500

    changes

--- link_funtain.php
+++ link_funtain.php
@@ -1,33 +1,29 @@
 <?php
 Class link_funtain
 {
-    public function createUser($ip,$mac,$single)
+    public function create_user_ws($ip,$mac)
     {
-        $online = 0;
-        $query_get_connected_user = "select user_id from user_funtain where online = 1;";
-        $user_online = $this->setConnection($query_get_connected_user);
-        $r1=mysqli_fetch_array($user_online,MYSQLI_NUM);
-        if($r1[0]==1)
+        $user_created = getExistUser($mac);
+        if($user_created>0)
         {
-            //someone connected
-            //create but not online
-            $online =0;
+            return $user_created;            
         }
         else
         {
-            //no one connected
-            //create online
-            $online =1;
+            $query_insert_user="INSERT INTO user_funtain (mac_adr, ip_adr, single, online) VALUES('$mac','$ip',0,0);";
+            $user_created = $this->setConnection($query_insert_user);  
+            return getExistUser($mac);          
         }
-        $query_cu="INSERT INTO user_funtain (mac_adr, ip_adr, online, single) VALUES('$mac','$ip',$online,$single);";
-        $user_created = $this->setConnection($query_cu);
-        //$r1=mysql_fetch_array($user_created,MYSQLI_NUM);
-        $query_last = "Select user_id from user_funtain where mac_adr = '$mac';";
-        $new_user = $this->setConnection($query_last);
-        $r_new_user=mysqli_fetch_array($new_user,MYSQLI_NUM);
-        if($r_new_user[0]>0)
+    }
+
+    private function getExistUser($mac)
+    {
+        $query_user = "Select user_id from user_funtain where mac_adr = '$mac';";
+        $exist_user = $this->setConnection($query_user);
+        $r_exist_user=mysqli_fetch_array($exist_user,MYSQLI_NUM);
+        if(sizeof($r_exist_user)>0)
         {
-            return $r_new_user[0];
+            return $r_exist_user[0];
         }
         else
         {
@@ -35,9 +31,110 @@ Class link_funtain
         }
     }
 
+    public function interaction_type_ws($user_id, $single)
+    {
+        $success = false;
+        $message = "";
+        if ($single == 1)
+        {
+            $connected_single =get_single_connected_fn();
+            if($connected_single==0)
+            {
+                $success=false;
+                $message="Single Player Already Online";
+            }
+            else
+            {
+                $query_online_single = "UPDATE user_funtain SET online =1, single=1 where user_id = '$user_id';";
+                $new_online = $this->setConnection($query_online_single);
+                $online = get_connected_by_id($user_id);       
+            }
+        }
+        else
+        {
+            $connected_multi = get_multi_connected_fn();
+            if ($connected_multi == 0)
+            {
+                $success=false;
+                $message="Multiplayer FULL";
+            }
+            else
+            {
+                $query_online_multi = "UPDATE user_funtain SET online =1, single=0 where user_id = '$user_id';";
+                $new_online = $this->setConnection($query_online_multi);
+                $online = get_connected_by_id($user_id);           
+            }
+        }
+
+        if($online ==1)//
+        {
+            $success=true;
+            $message = "Connected!";
+        }
+        else
+        {
+            $success=false;
+            $message="Connection Error.";
+        }
+        return setJsonResponse($sucess,$message);
+    }
+
+    private function get_single_connected_fn()
+    {
+        $online = 0;
+        $query_get_connected_single = "select user_id from user_funtain where online = 1 and single = 1;";
+        $single_online = $this->setConnection($query_get_connected_single);
+        $r1=mysqli_fetch_array($single_online,MYSQLI_NUM);
+        if (sizeof($r1) > 0)
+        {
+            //hay un single conectado
+            return 0;
+        }
+        else
+        {
+            //no hay single conectado
+            return 1;
+        }
+    }
+
+    private function get_multi_connected_fn()
+    {
+        //validar maximo 5 conectados
+        $query_get_connected_multi = "select user_id from user_funtain where online = 1 and single = 0;";
+        $multi_online = $this->setConnection($query_get_connected_multi);
+        $r1=mysqli_fetch_array($single_online,MYSQLI_NUM);
+        if(sizeof($r1)<5)
+        {
+            //multi todavia hay espacio
+            return 1;
+        }
+        else
+        {
+            //multi completado
+            return 0;
+        }
+    }
+
+    private function get_connected_by_id($user_id)
+    {
+        $query_get_connected = "select online from user_funtain where user_id = $user_id;";
+        $r_get_connected = $this->setConnection($query_get_connected);
+        $rc=mysqli_fetch_array($r_get_connected,MYSQLI_NUM);
+        if(sizeof($rc)>0)
+        {
+            //connected success
+            return 1;
+        }
+        else
+        {
+            //not connected error
+            return 0;
+        }
+    }
+
     public function setOffline($user)
     {
-        $query_offline = "update user_id set online = 0 where user_id = '$user';";
+        $query_offline = "update user_funtain set online = 0 where user_id = '$user';";
         $this->setConnection($query_offline);
     }
 
@@ -55,19 +152,51 @@ Class link_funtain
 		return $response;
     }
 
-    public function insertShakeValue($user,$value,$single)
+    public function insert_single_ws($user_id, $value)
     {
-        $online_users = json_decode( getOnline($user));
+        $online = get_connected_by_id($user_id);
+        if($online ==1)//user connected
+        {
+            $message = insertShakeValue($user_id,$value,1);
+            $success=true;
+            //$message = "Inserted Value!";
+        }
+        else
+        {
+            $success=false;
+            $message="User not Online";
+        }
+        return setJsonResponse($sucess,$message);
+    }
+
+    public function insert_multi_ws($user_id, $value)
+    {
+        $online = get_connected_by_id($user_id);
+        if($online ==1)//user connected
+        {
+            $message=insertShakeValue($user_id,$value,0);
+            $success=true;
+            //$message = "Inserted Value!";
+        }
+        else
+        {
+            $success=false;
+            $message="User not Online";
+        }
+        return setJsonResponse($sucess,$message);
+    }
+
+    private function insertShakeValue($user,$value,$single)
+    {
         if($single )
         {
-            $query_shake = "INSERT INTO single_shake (user_id, shake_val) VALUES($user,$value);";
-            $resp_shake = setConnection($query_shake);            
+            $query_shake = "INSERT INTO single_shake (user_id, shake_val) VALUES($user,$value);";            
         }
         else
         {
             $query_shake = "INSERT INTO group_shake (user_id, shake_val) VALUES($user,$value);";
-            $resp_shake = setConnection($query_shake);
         }
+        return setConnection($query_shake);
     }
 
     private function setConnection($con_query)
@@ -79,6 +208,16 @@ Class link_funtain
 		$result=mysqli_query($con,$con_query) or die ("request error" );
 		mysqli_close($con);
 		return $result;
+    }
+
+    private function setJsonResponse($success, $message)
+    {
+        $myObj->success = $success;
+        $myObj->message = $message;
+        
+        $myJSON = json_encode($myObj);
+        
+        return $myJSON;
     }
 }
 ?>
--- svc_funtain.php
+++ svc_funtain.php
@@ -6,24 +6,46 @@ require_once("link_funtain.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $Req_Met = $_REQUEST['metodo'] or die('No existe ese metodo');
+    //metodos de seteo
+    $Req_Met = $_REQUEST['metodo'] or die('Falta metodo');
     if($Req_Met == "registrar")
     {
         $mac_adrs = $_POST['mac'];
         $ip_adrs = $_POST['ip'];
+        $ob_link_fun = new link_funtain();
+    	$response = $ob_link_fun->create_user_ws($ip_adrs, $mac_adrs);
+        print $response;
+    }
+    else if ($Req_Met == "interaction")
+    {
+        //si single =1, juego solitario, valida si existe alguien mas conectado
+        //retorna parametro y empieza o no juego
+        $user_id = $_POST['user_id'];
         $single = $_POST['single'];
         $ob_link_fun = new link_funtain();
-    	$response = $ob_link_fun->createUser($ip_adrs, $mac_adrs, $single);
+    	$response = $ob_link_fun->interaction_type_ws($user_id, $single);
         print $response;
+    }
+    else if ($Req_Met == "desconectar")
+    {
+        $user_id = $_POST['user_id'];
+        $ob_link_fun = new link_funtain();
+    	$response = $ob_link_fun->setOffline($user_id);
+        print $response;
+    }
+    else
+    {
+        die ('no existe ese metodo');
     }
 }
 else
 {
+    //metodos de envio de data
     $Req_Met = $_REQUEST['metodo'] or die('No existe ese metodo');
-    if($Req_Met == "sendvalue")
+    if($Req_Met == "shakeSingle")
     {
         $req_value = $_REQUEST['value'];
-        $req_user = $_REQUEST['user'];
+        $req_user = $_REQUEST['user_id'];
         if(!isset($req_value) or !isset($req_user))
         {
             return "Falta valor.";
@@ -31,9 +53,28 @@ else
         else
         {
             $ob_link_fun = new link_funtain();
-            $response = $ob_link_fun->insertShakeValue($req_user,$req_value);
+            $response = $ob_link_fun->insert_single_ws($req_user,$req_value);
             print $response;
         }
+    }
+    else if ($Req_Met == "shakeMulti")
+    {
+        $req_value = $_REQUEST['value'];
+        $req_user = $_REQUEST['user_id'];
+        if(!isset($req_value) or !isset($req_user))
+        {
+            return "Falta valor.";
+        }
+        else
+        {
+            $ob_link_fun = new link_funtain();
+            $response = $ob_link_fun->insert_multi_ws($req_user,$req_value);
+            print $response;
+        }
+    }
+    else
+    {
+        die ('no existe ese metodo');
     }
 }
 ?>
