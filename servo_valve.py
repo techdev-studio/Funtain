e495d08023a72baf27aac06e3bfd35d2f50c240b
techdev-studio <info@techdev-studio.com>
2018-01-24 06:09:40 +0000

    automated servo reading database

--- servo_valve.py
+++ servo_valve.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO # Cargamos la libreria RPi.GPIO
 from time import sleep  # cargamo
+import MySQLdb
 
 GPIO.setmode(GPIO.BCM)  # Ponemos la Raspberry en modo BCM
 
@@ -7,18 +8,38 @@ GPIO.setup(18, GPIO.OUT)  # Ponemos el pin GPIO como salida
 
 valve=GPIO.PWM(18, 100)
 
-vale.start(0)
+valve.start(0)
+pause_time = 0.5           # Declaramos un lapso de tiempo para las pausas
 
-pause_time = 0.02           # Declaramos un lapso de tiempo para las pausas
+max_value = 1000
 
 try:                        # Abrimos un bloque 'Try...except KeyboardInterrupt'
     while True:             # Iniciamos un bucle 'while true'
-        for i in range(0,101):            # De i=0 hasta i=101 (101 porque el script se detiene al 100%)
-            valve.ChangeDutyCycle(i)      # LED #1 = i
-            sleep(pause_time)             # Pequena pausa para no saturar el procesador
-        for i in range(100,-1,-1):        # Desde i=100 a i=0 en pasos de -1
-            valve.ChangeDutyCycle(i)      # LED #1 = i
-            sleep(pause_time)             # Pequena pausa para no saturar el procesador
+        db = MySQLdb.connect(host="localhost", user="root", passwd="funtain@tds", db="funtain_db")
+
+	#create a cursor for the select
+	cur = db.cursor()
+
+	#execute an sql query
+	cur.execute("SELECT *  FROM single_shake where shaked = 0 order by sshake_id asc;")
+
+	##Iterate 
+	for row in cur.fetchall() :
+      		#data from rows
+        	sshake_id = row[0] 
+        	shake_val = row[2]
+        	cur.execute("update single_shake set shaked=1 where sshake_id = " + str(sshake_id))
+		db.commit()
+      		#print 
+        	print shake_val            
+        	valve.ChangeDutyCycle(100*(max_value-shake_val)/max_value)
+        	sleep(pause_time)
+
+	# close the cursor
+	cur.close()
+
+	# close the connection
+	db.close ()
 
 except KeyboardInterrupt:   # Se ha pulsado CTRL+C!!
     valve.stop()            # Detenemos el objeto 'valve'
